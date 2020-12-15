144f5ab6a17044d67fe4e52885ea43f2659391e9
Larissa Santana <lariissasantana_n@hotmail.com>
2020-12-15 16:10:53 -0300

    adicionando BaseEntity

---
+++ CopaDeFilmes/CopaDeFilmes.Domain/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace CopaDeFilmes.Domain
+{
+    public abstract class BaseEntity<T> : AbstractValidator<T> where T : BaseEntity<T>
+    {
+        public ValidationResult ValidationResult { get; protected set; }
+        protected BaseEntity()
+        {
+            ValidationResult = new ValidationResult();
+        }
+
+        public abstract bool IsValid();
+    }
+}
--- CopaDeFilmes/CopaDeFilmes.Domain/Entities/Filme.cs
+++ CopaDeFilmes/CopaDeFilmes.Domain/Entities/Filme.cs
@@ -1,10 +1,16 @@
 ﻿namespace CopaDeFilmes.Domain.Entities
 {
-    public class Filme
+    public class Filme : BaseEntity<Filme>
     {
         public string Id { get; private set; }
         public string Titulo { get; private set; }
         public int Ano { get; private set; }
         public float Nota { get; private set; }
+
+        public override bool IsValid()
+        {
+            ValidationResult = Validate(this);
+            return ValidationResult.IsValid;
+        }
     }
 }
