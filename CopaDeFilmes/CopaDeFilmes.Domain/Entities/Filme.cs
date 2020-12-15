d7761cb767941947a916b5d5209c0a35bd5fb43d
Larissa Santana <lariissasantana_n@hotmail.com>
2020-12-15 14:26:56 -0300

    Criando projeto de domínio e entidade Filme.

---
+++ CopaDeFilmes/CopaDeFilmes.Domain/Entities/Filme.cs
@@ -0,0 +1,10 @@
+﻿namespace CopaDeFilmes.Domain.Entities
+{
+    public class Filme
+    {
+        public string Id { get; private set; }
+        public string Titulo { get; private set; }
+        public int Ano { get; private set; }
+        public float Nota { get; private set; }
+    }
+}
