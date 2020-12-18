20774a2cb8a449e6f74d70317cb02ecbd6bbf78f
Larissa Santana <lariissasantana_n@hotmail.com>
2020-12-17 22:29:51 -0300

    Criando Mocks para os filmes.

---
+++ CopaDeFilmes/Tests/UnitTests/CopaDeFilmes.Domain.Tests/FilmesMock.cs
@@ -0,0 +1,73 @@
+﻿using CopaDeFilmes.Domain.Entities;
+using static CopaDeFilmes.Domain.Entities.Filme;
+
+namespace CopaDeFilmes.Domain.Tests
+{
+    public static class FilmesMock
+    {
+        public static Filme OsIncriveis2 = FilmeFactory.Create
+            (
+                id: "tt3606756",
+                titulo: "Os Incríveis 2",
+                ano: 2018,
+                nota: 8.5f
+            );
+
+        public static Filme JurassicWorld = FilmeFactory.Create
+            (
+                id: "tt4881806",
+                titulo: "Jurassic World: Reino Ameaçado",
+                ano: 2018,
+                nota: 6.7f
+            );
+
+        public static Filme OitoMulheresEUmSegredo = FilmeFactory.Create
+            (
+                id: "tt5164214",
+                titulo: "Oito Mulheres e um Segredo",
+                ano: 2018,
+                nota: 6.3f
+            );
+
+        public static Filme Hereditario = FilmeFactory.Create
+            (
+                id: "tt7784604",
+                titulo: "Hereditário",
+                ano: 2018,
+                nota: 7.8f
+            );
+
+        public static Filme Vingadores = FilmeFactory.Create
+            (
+                id: "tt4154756",
+                titulo: "Vingadores: Guerra Infinita",
+                ano: 2018,
+                nota: 8.8f
+            );
+
+        public static Filme Deadpool2 = FilmeFactory.Create
+            (
+                id: "tt5463162",
+                titulo: "Deadpool 2",
+                ano: 2018,
+                nota: 8.1f
+            );
+
+        public static Filme HanSolo = FilmeFactory.Create
+            (
+                id: "tt3778644",
+                titulo: "Han Solo: Uma História Star Wars",
+                ano: 2018,
+                nota: 7.2f
+            );
+
+        public static Filme ThorRagnarok = FilmeFactory.Create
+           (
+               id: "tt3501632",
+               titulo: "Thor: Ragnarok",
+               ano: 2017,
+               nota: 7.9f
+           );
+    }
+
+}
