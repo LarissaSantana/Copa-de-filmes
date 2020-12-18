a1358919200fe14dd319dce1e7eec110fdde0a71
Larissa Santana <lariissasantana_n@hotmail.com>
2020-12-17 21:38:13 -0300

    Criando a classe de teste.

---
+++ CopaDeFilmes/Tests/UnitTests/CopaDeFilmes.Domain.Tests/FilmeServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using CopaDeFilmes.Domain.Service;
+using Xunit;
+
+namespace CopaDeFilmes.Domain.Tests
+{
+    [Collection(nameof(FilmeServiceCollection))]
+    public class FilmeServiceTests
+    {
+        private readonly FilmeService _filmeService;
+        private readonly FilmeServiceFixture _filmeServiceFixture;
+
+        public FilmeServiceTests(FilmeServiceFixture filmeServiceFixture)
+        {
+            _filmeServiceFixture = filmeServiceFixture;
+            _filmeService = _filmeServiceFixture.GetFilmeService();
+        }
+
+
+    }
+}
