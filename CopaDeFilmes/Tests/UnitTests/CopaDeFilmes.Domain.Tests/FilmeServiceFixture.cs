75e8a99a82e7cd6d28a7bf91fb5a7cd853a1288a
Larissa Santana <lariissasantana_n@hotmail.com>
2020-12-17 21:34:45 -0300

    Criando FilmeServiceFixture.

---
+++ CopaDeFilmes/Tests/UnitTests/CopaDeFilmes.Domain.Tests/FilmeServiceFixture.cs
@@ -0,0 +1,20 @@
+﻿using CopaDeFilmes.Domain.Service;
+using Moq.AutoMock;
+using Xunit;
+
+namespace CopaDeFilmes.Domain.Tests
+{
+    [CollectionDefinition(nameof(FilmeServiceCollection))]
+    public class FilmeServiceCollection : ICollectionFixture<FilmeServiceFixture> { }
+    public class FilmeServiceFixture
+    {
+        public FilmeService FilmeService;
+        public AutoMocker Mocker;
+
+        public FilmeService GetFilmeService()
+        {
+            Mocker = new AutoMocker();
+            return Mocker.CreateInstance<FilmeService>();
+        }
+    }
+}
