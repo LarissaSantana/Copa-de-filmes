dcd708f9745304d2b91045bf5ebbd583ec8e8ed5
Larissa Santana <lariissasantana_n@hotmail.com>
2020-12-18 12:45:16 -0300

    Criando estrutura para testes do Application.

---
+++ CopaDeFilmes/Tests/UnitTests/CopaDeFilmes.Application.Tests/FilmeAppServiceFixture.cs
@@ -0,0 +1,20 @@
+﻿using CopaDeFilmes.Application.Services;
+using Moq.AutoMock;
+using Xunit;
+
+namespace CopaDeFilmes.Application.Tests
+{
+    [CollectionDefinition(nameof(FilmeAppServiceCollection))]
+    public class FilmeAppServiceCollection : ICollectionFixture<FilmeAppServiceFixture> { }
+    public class FilmeAppServiceFixture
+    {
+        public FilmeAppService FilmeAppService;
+        public AutoMocker Mocker;
+
+        public FilmeAppService GetFilmeAppService()
+        {
+            Mocker = new AutoMocker();
+            return Mocker.CreateInstance<FilmeAppService>();
+        }
+    }
+}
